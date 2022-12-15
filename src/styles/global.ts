5cb81461d58ad744dd5154c44aa43d1940181556
srod-douglas <srod.douglas@gmail.com>
2022-12-14 22:32:22 -0300

    feat: add styled-components and reset from globalstyles

---
+++ src/styles/global.ts
@@ -0,0 +1,12 @@
+import { CreateGlobalStyles } from 'styled-components'
+
+const GlobalStyle = CreateGlobalStyles`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+        list-style: none;
+        text-decoration: none;
+    }
+`
